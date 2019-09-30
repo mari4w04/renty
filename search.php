6e0c70d60151a3e5b72e3a6771f385bb3898f02e
Charlene Marteyn <charlenemarteyn@Charlenes-MacBook-Pro.local>
2019-09-30 14:14:25 +0200

    test commit

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/top.php'; 
+?>
+
+<h1> choose your type of bikes</h1>
+
+<div class="bikeCategoryContainer">
+    <div>
+        <img src="images/damecykler-ungkvinde-classic-new.jpg">
+        <p>Lady bikes</p>
+    </div>
+    <div></div>
+</div>
+
+
+
+
+<?php
+require_once __DIR__.'/bottom.php'; 
