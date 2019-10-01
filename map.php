c6a11748a99a07ea8c2fbee74abbd987dc82297f
Charlene Marteyn <charlenemarteyn@laptop-8pei8gbv.kea.local>
2019-10-01 10:15:19 +0200

    top and bottom added

---
+++ map.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__.'/top.php'; 
+?>
+
+
+
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.0472940159148!2d12.536451415777574!3d55.70554968054066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46525248e4e905a1%3A0xb8251ef4e0ba6c3f!2sLygten%208%2C%202400%20K%C3%B8benhavn!5e0!3m2!1sda!2sdk!4v1569916105443!5m2!1sda!2sdk" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+<?php
+require_once __DIR__.'/bottom.php'; 
--- search.php
+++ search.php
@@ -3,14 +3,17 @@
 require_once __DIR__.'/top.php'; 
 ?>
 
-<h1> choose your type of bikes</h1>
+<h1> choose your type of bikes :</h1>
 
 <div class="bikeCategoryContainer">
     <div>
         <img src="images/damecykler-ungkvinde-classic-new.jpg">
         <p>Lady bikes</p>
     </div>
-    <div></div>
+    <div>
+        <img src="images/damecykler-ungkvinde-classic-new.jpg">
+        <p>Lady bikes</p>
+    </div>
 </div>
 
 
