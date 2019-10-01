7114befb31ef8a92c878cd569463f0d19f6b7763
ioana <ioanav3796@gmail.com>
2019-10-01 13:50:37 +0200

    payment page + ongoing ride page with timer

---
+++ js/validate.js
@@ -0,0 +1,38 @@
+$('#frmPayment').submit( function(){
+
+    $.ajax({
+        method:"POST",
+        url:"apis/api-payment.php",
+        data: $('#frmPayment').serialize(),
+        dataType:"JSON"
+    }).done(function(jData){
+        console.log(jData)
+        if(jData.status == 1){
+            location.href = 'ride-start.php';
+            // swal({
+            //     title: "Payment success",
+            //     text: "You can login now",
+            //     icon: "success",
+            //     buttons: {
+            //         backToLogin: {
+            //           text: "Close",
+            //           value: "backToLogin",
+            //         },
+            //       },
+            // })
+            // .then(function() {
+            //     location.href = 'login';
+            //   }) 
+        }else{
+            swal({
+                title: jData.message,
+                // text: "Please re-type it",
+                icon: "warning",
+            });
+        }
+    }).fail(function(){
+        console.log('API does not work')
+    });
+
+    return false
+})
