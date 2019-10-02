339020084141f013ca793d96f47044af9137daa4
Amalia Plana <amalia@penni.io>
2019-10-02 20:15:16 +0200

    styling

--- search.php
+++ search.php
@@ -2,26 +2,31 @@
 
 require_once __DIR__.'/top.php'; 
 ?>
+<div class="page">
+<div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
 <nav>
-        <img src="images/back.png" alt="">
-        <img src="images/logo-pink.png" alt="">
+        <img src="images/back-white.png" alt="">
+        <img src="images/logo.png" alt="">
         <div>Logout</div>
     </nav>
 
 <div class="search-page">
 
-<h1> Which type of bike?</h1>
+<h1> Chose your bike</h1>
 
 <div class="bikeCategoryContainer">
-    <div>
-        <img src="images/damecykler-ungkvinde-classic-new.jpg">
-        <p>Lady bikes</p>
+    <div class="container">
+        <img src="images/funky-bike1.png">
+        <h2>Lady bikes</h2>
     </div>
-    <div>
-        <img src="images/damecykler-ungkvinde-classic-new.jpg">
-        <p>Lady bikes</p>
+    <div class="container">
+        <img src="images/funky-bike2.png">
+        <h2>Lady bikes</h2>
         
     </div>
+</div>
 </div>
 </div>
 
