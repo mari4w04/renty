3fe8cdb33db80e976a6fc25629f32f0d01aab412
Charlene Marteyn <charlenemarteyn@laptop-8pei8gbv.kea.local>
2019-10-03 16:43:10 +0200

    image carousel added

--- map.php
+++ map.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="app.css">
-    <title>Map</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="app.css">
+<title>Map</title>
 </head>
-<body>
-<div id="clicker"></div>
-<div id="up-arrow"></div>
-<div id="mapContainer">
-<img id="map" src="images/Screenshot 2019-10-01 at 11.03.21.png">
-<div id="bikeInfo">
-<p class="address">Vibevej 8</p>
-<p class="distance">197 M AWAY</p>
-<hr>
-<img src="images/norden-ellen-da-n7-askegra-m-pink.png">
+<body id="mapPage">
+    <div id="clicker"></div>
 
-</div>
+    <div id="up-arrow"></div>
 
-</div>
+    <div id="bikeInfo">
+        <p class="address">Vibevej 8</p>
+        <p class="distance">197 M AWAY</p>
+        <hr>
+        <img src="images/norden-ellen-da-n7-askegra-m-pink.png">
+    </div>
+
+    <a id="chooseBikeBtn" href="#"><button>Choose</button></a>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 
