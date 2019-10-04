243517d41ce9c22ae22f0ec11cea44960c05487b
Marija <mari4w04@stud.kea.dk>
2019-10-04 19:55:10 +0200

    changes

--- confirmation.php
+++ confirmation.php
@@ -25,27 +25,27 @@
 <!-- map -->
 <div class="confirmation-section">
 <!-- km ridden -->
-<div>
-<img src="images/pin.png" alt="">
-<h6>Distance</h6>
-<h4>1km</h4>
-</div>
+    <div>
+        <img src="images/pin.png" alt="">
+        <h6>Distance</h6>
+        <h4>1km</h4>
+    </div>
 <!-- time ride -->
-<div>
-<img src="images/stop-watch.png" alt="">
-<h6>Duration</h6>
-<h4>3min</h4>
-</div>
-<!-- date, start time finish time -->
-<div>
-<h4>Tuesday, October 1, 2019</h4>
-<h6>5.50pm-5.53</h6>
-</div>
-<!-- total price -->
-<div class="confirmation-price">
-<h4>18kr</h4>
-<h6>christiania bike</h6>
-</div>
+    <div>
+        <img src="images/stop-watch.png" alt="">
+        <h6>Duration</h6>
+        <h4>3min</h4>
+    </div>
+    <!-- date, start time finish time -->
+    <div>
+        <h4>Tuesday, October 1, 2019</h4>
+        <h6>5.50pm-5.53</h6>
+    </div>
+    <!-- total price -->
+    <div class="confirmation-price">
+        <h4>18kr</h4>
+        <h6>christiania bike</h6>
+    </div>
 </div>
 <a href='map.php'><button>New ride</button></a>
 
--- payment.php
+++ payment.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/background.css">
     <link rel="stylesheet" href="css/white-theme.css">
+    <link rel="stylesheet" href="css/overall-style.css">
     <title>Renty</title>
 </head>
 <body>
@@ -19,32 +20,50 @@
         <a href="scan"><img src="images/back-white.png" alt=""></a>
     </nav>
 
-    <h1 class="padding-top-bottom-40">Payment</h1>
- 
-        <div class="payment-info">
-        <h2>10kr unlock + 2.50kr / min<span class="align-right" id="chosenCarPrice"></span></h2>
-        <h2><input type="checkbox" checked>Insurance 25kr</h2>
-        </div>
-        <div class="container">
+    <div class="login-page-wrapper">
+    <h1 class="payment-h1">Just ride</h1>
+    <p class="payment-p">Just ride and pay at the end. <br/>
+The longer you ride the cheaper it gets.</p>
+        
+        <div class="white-container payment-container">
         <!-- <h2>Credit Card & Billing Information</h2> -->
         <form id="frmPayment" method="POST">
-            <h6>Card Number</h6>
-            <input name="paymentCardNo" type="text" id="paymentCardNo">
-            <h6>Card Holder</h6>
-            <input name="paymentCardHolder" type="text" id="paymentCardHolder">
+
+            
+            
+            
+            
             <div class="card-expiry-cvv">
-                <div class="expiry-container">
-                    <h6>Month</h6>
-                    <h6>Year</h6>
-                    <input name="paymentMonth" type="text" id="paymentCardExpiryMonth">
-                    <input name="paymentYear" type="text" id="paymentCardExpiryYear">
+                <div class="card-number-box">
+                    <i class="far fa-credit-card"></i>
+                    <input name="paymentCardNo" type="text" id="paymentCardNo" placeholder="1234 1234 1234 1234">
                 </div>
-                <h6>CVV</h6>
-                <input name="paymentCvv" id="paymentCvv" type="text">
+                <div class="expiry-container">
+                    <input name="paymentMonth" type="text" id="paymentCardExpiryMonth" placeholder="MM">
+                    <span>/</span>
+                    <input name="paymentYear" type="text" id="paymentCardExpiryYear" placeholder="YY">
+                </div>
+                
+                <input name="paymentCvv" id="paymentCvv" type="text" placeholder="CVV">
             </div>
-        
-            <button id="payBtn" type="submit">Pay</button>
+
+            <h6>Cardholder name</h6>
+            <input name="paymentCardHolder" type="text" id="paymentCardHolder">
+
+            <div class="payment-info">
+                <h5 class="red-h5">Rates</h5>
+                <p class="rate-info">10 kr. unlock + 2.50 kr./min<span class="align-right" id="chosenCarPrice"></span></p>
+                <p class="insurance">
+                <label class="container-checkbox insurance" >I would like to have insurance for 25 kr.
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>  
+                </p>
+            </div>
+            <button id="payBtn" type="submit">Save</button>
+
+            
         </form>
+</div>
    </div>
     
 
@@ -53,6 +72,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="js/validate.js"></script>
+<script src="https://kit.fontawesome.com/22eaa1461b.js" crossorigin="anonymous"></script>
 
 
 </body>
